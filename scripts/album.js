89b7978db25bfe77cdf332cf4123e415a34b0d6a
John Howard <longhornjoho@gmail.com>
2016-08-31 02:19:15 +0000

    assignment33: complete, play/pause bar

--- scripts/album.js
+++ scripts/album.js
@@ -51,7 +51,7 @@ var createSongRow = function(songNumber, songName, songLength) {
         } else if (currentlyPlayingSongNumber === songNumber) {
             if (currentSoundFile.isPaused()) {
             $(this).html(pauseButtonTemplate);
-            $('.main-controls .play-pause').html(playerBarPauseButtonn);
+            $('.main-controls .play-pause').html(playerBarPauseButton);
             currentSoundFile.play();
             } else {
             $(this).html(playButtonTemplate);
@@ -115,7 +115,7 @@ var updatePlayerBarSong = function() {
     $('.currently-playing .song-name').text(currentSongFromAlbum.title);
     $('.currently-playing .artist-name').text(currentAlbum.artist);
     $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
-    $('.main-controls .play-pause').html(playerBarPauseButtonn);
+    $('.main-controls .play-pause').html(playerBarPauseButton);
 };
 
 var nextSong = function() {
@@ -174,25 +174,48 @@ var previousSong = function() {
     $lastSongNumberCell.html(lastSongNumber);
 };
 
+var togglePlayFromPlayerBar = function() {
+    //var songNumber = parseInt($(this).attr('data-song-number'));
+    if (currentlyPlayingSongNumber === null) {
+        currentlyPlayingSongNumber = 1;
+        getSong(currentlyPlayingSongNumber);
+    }
+    var $currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+    
+    if (currentSoundFile.isPaused()) {
+        $currentlyPlayingCell.html(pauseButtonTemplate);
+        $(this).html(playerBarPauseButton);
+        getSong(currentlyPlayingSongNumber);
+        currentSoundFile.play();
+    } else if (currentSoundFile) {
+        $currentlyPlayingCell.html(playButtonTemplate);
+        $(this).html(playerBarPlayButton);
+        getSong(currentlyPlayingSongNumber);
+        currentSoundFile.pause();
+    }
+};
+
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var playerBarPlayButton = '<span class="ion-play"></span>';
-var playerBarPauseButtonn = '<span class="ion-pause"></span>';
+var playerBarPauseButton = '<span class="ion-pause"></span>';
 
 // store state of playing song
 var currentAlbum = null;
 var currentlyPlayingSongNumber = null;
 var currentSongFromAlbum = null;
 var currentSoundFile = null;
-var currentVolume = 80;
+var currentVolume = 1;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
+    $playPauseButton.click(togglePlayFromPlayerBar);
 });
 
 
